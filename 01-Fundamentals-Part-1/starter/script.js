e66115f77a925a41b2a09d0faf04e01d0e3ae0d7
rebecca <185906896+trustnoreb@users.noreply.github.com>
2024-10-25 11:26:39 +0200

    fine giornata

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -3,3 +3,9 @@ if(js==='amazing') alert('Javascript is FUN')
 
 40 + 8 + 10 + 33 
 console.log(40 + 8 + 10 + 33);
+
+console.log("Jonas");
+console.log(99);
+
+let firstName = "Bob";
+console.log(firstName);
