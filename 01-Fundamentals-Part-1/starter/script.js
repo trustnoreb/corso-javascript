9d6d51c87ec3459537a0e414ff7ceee27622461b
rebecca <185906896+trustnoreb@users.noreply.github.com>
2024-10-28 18:00:22 +0100

    datatypes

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,11 +1,23 @@
-let js='amazing';
-if(js==='amazing') alert('Javascript is FUN')
+// let js='amazing';
+// if(js==='amazing') alert('Javascript is FUN')
 
-40 + 8 + 10 + 33 
-console.log(40 + 8 + 10 + 33);
+// 40 + 8 + 10 + 33 
+// console.log(40 + 8 + 10 + 33);
 
-console.log("Jonas");
-console.log(99);
+// console.log("Jonas");
+// console.log(99);
 
-let firstName = "Bob";
-console.log(firstName);
+// let firstName = "Bob";
+// console.log(firstName);
+
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+
+console.log(typeof true);
+
+//dynamic typing
+javascriptIsFun = 3;
+console.log(typeof javascriptIsFun);
+
+javascriptIsFun = 'Bob';
+console.log(typeof javascriptIsFun);
