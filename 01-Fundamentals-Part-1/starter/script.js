3f445b595bbc447cfcb59fc22cb0d683ad4849d0
rebecca <185906896+trustnoreb@users.noreply.github.com>
2024-11-02 19:43:59 +0100

    Switch statement

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -235,3 +235,45 @@ if(hasDriverLicence && hasGoodVision && !isTired){
 } else {
     console.log(`Someone else should drive..`);
 }
+//----------------------------------------------------------
+
+//THE SWITCH STATEMENT
+const day = 'friday';
+
+switch(day){
+    case 'monday': // day === 'monday'
+        console.log('Plan my course structure');
+        console.log('Go to cooding');
+        break;
+    case 'tuesday':
+        console.log('Prepare theory videos');
+        break;
+    case 'wednesday':
+    case 'thursday':
+        console.log('Write code examples');
+        break;
+    case 'friday':
+        console.log('Record videos');
+        break;
+    case 'saturday':
+    case 'sunday':
+        console.log('enjoy the weekend');
+        break;
+    default:
+        console.log('Not a valid day!');
+}
+
+if(day === 'monday'){
+    console.log('Plan my course structure');
+    console.log('Go to cooding');
+} else if (day === 'tuesday'){
+    console.log('Prepare theory videos');
+} else if (day === 'wednesday' || day === 'thursday'){
+    console.log('Write code examples');
+} else if (day === 'friday'){
+    console.log('Record videos');
+} else if (day === 'saturday' || day === 'sunday'){
+    console.log('Enjoy the weekend');
+} else {
+    console.log('Not a valid day!');
+}
