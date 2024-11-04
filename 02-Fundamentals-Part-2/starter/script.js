cb9fb2273c44b68e5234830a4557360433a777cc
rebecca <185906896+trustnoreb@users.noreply.github.com>
2024-11-04 14:58:01 +0100

    Activating Strict Mode

---
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -0,0 +1,16 @@
+// ACTIVATING STRICT MODE
+'use strict';
+
+let hasDriversLicence = true;
+const passTest = true;
+
+if(passTest) hasDriverLicence = true;
+if(hasDriversLicence) console.log('I can drive :D');
+
+// const interface = 'Audio'; // errore perchè interface è riservata
+// const private = 534;
+// const if = 23;
+
+//----------------------------------------------------------
+
+// FUNCTIONS
