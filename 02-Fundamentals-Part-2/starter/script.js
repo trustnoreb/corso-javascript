f76499b3b189bf6c950df143b44c713b141105d5
rebecca <185906896+trustnoreb@users.noreply.github.com>
2024-11-07 15:31:58 +0100

    The while loop

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -241,62 +241,85 @@
 //----------------------------------------------------------
 
 // LOOPING ARRAY, BREAKING AND CONTINUE
-const jonasArray = [
-    'Jonas',
-    'Schmedtmann',
-    2037 - 1991,
-    'teacher',
-    ['Michael', 'Peter', 'Steve'],
-    true
-];
+// const jonasArray = [
+//     'Jonas',
+//     'Schmedtmann',
+//     2037 - 1991,
+//     'teacher',
+//     ['Michael', 'Peter', 'Steve'],
+//     true
+// ];
 
-let types = [];
+// let types = [];
 
-for (let i = 0; i < jonasArray.length; i++ ){
-    console.log(jonasArray[i], typeof jonasArray[i]);
+// for (let i = 0; i < jonasArray.length; i++ ){
+//     console.log(jonasArray[i], typeof jonasArray[i]);
 
     //Filling types array
     //types[i] = typeof jonasArray[i];
-    types.push(typeof jonasArray[i]);
-}
+//     types.push(typeof jonasArray[i]);
+// }
 
-console.log(types);
+// console.log(types);
 
-const years = [1991, 2007, 1969, 2020];
-const ages = [];
-for(let i=0; i< years.length; i++){
-    ages.push(2037 - years[i]);
-}
+// const years = [1991, 2007, 1969, 2020];
+// const ages = [];
+// for(let i=0; i< years.length; i++){
+//     ages.push(2037 - years[i]);
+// }
 
-console.log(ages);
+// console.log(ages);
 
 //continue and break
-console.log('---ONLY STRINGS---');
-for (let i = 0; i < jonasArray.length; i++ ){
-    if(typeof jonasArray[i] !== 'string') continue;
-    console.log(jonasArray[i], typeof jonasArray[i]);
-}
+// console.log('---ONLY STRINGS---');
+// for (let i = 0; i < jonasArray.length; i++ ){
+//     if(typeof jonasArray[i] !== 'string') continue;
+//     console.log(jonasArray[i], typeof jonasArray[i]);
+// }
 
-console.log('---BREAK WITH NUMBER---');
-for (let i = 0; i < jonasArray.length; i++ ){
-    if(typeof jonasArray[i] === 'number') break;
-    console.log(jonasArray[i], typeof jonasArray[i]);
-}
+// console.log('---BREAK WITH NUMBER---');
+// for (let i = 0; i < jonasArray.length; i++ ){
+//     if(typeof jonasArray[i] === 'number') break;
+//     console.log(jonasArray[i], typeof jonasArray[i]);
+// }
 
 //----------------------------------------------------------
 
 //LOOPING BACKWARDS AND LOOPS IN LOOPS
 
 //Looping Backwards
-for( let i = jonasArray.length -1; i >= 0; i--){
-    console.log(i, jonasArray[i]);
-}
+// for( let i = jonasArray.length -1; i >= 0; i--){
+//     console.log(i, jonasArray[i]);
+// }
 
 //Loops in Loops
-for(let exercise = 1; exercise < 4; exercise++){
-  console.log(`--STARTING EXERCICE ${exercise}--`);  
+// for(let exercise = 1; exercise < 4; exercise++){
+//   console.log(`--STARTING EXERCICE ${exercise}--`);  
 
-    for(let rep = 1; rep < 6; rep ++ ){
-        console.log(`Exercice ${exercise}: Lifting weight repetition ${rep}`);
-    }
+//     for(let rep = 1; rep < 6; rep ++ ){
+//         console.log(`Exercice ${exercise}: Lifting weight repetition ${rep}`);
+//     }
+// }
+
+//----------------------------------------------------------
+
+//THE WHILE LOOP
+
+// for(let rep = 1; rep <= 10; rep++){
+//     console.log(`FOR: Lifting weights repetition ${rep}`);
+// }
+
+// let rep = 1;
+// while(rep <=10){
+//     console.log(`WHILE: Lifting weights repetition ${rep}`);
+//     rep++;
+// }
+
+let dice = Math.trunc(Math.random()*6) +1 ;
+console.log(dice);
+
+while (dice !== 6){
+    console.log(`You rolled a ${dice}`);
+    dice = Math.trunc(Math.random()*6) +1 ;
+    if(dice === 6) console.log('Loop is about to end..');
 }
