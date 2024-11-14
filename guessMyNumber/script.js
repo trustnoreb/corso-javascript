d352e60914b2b1833271bbaaece83b01622c5ac1
rebecca <185906896+trustnoreb@users.noreply.github.com>
2024-11-14 16:11:05 +0100

    Highscore function

--- guessMyNumber/script.js
+++ guessMyNumber/script.js
@@ -11,6 +11,7 @@
 let secretNumber = Math.trunc(Math.random() * 20) + 1;
 
 let score = 20;
+let highscore = 0;
 
 document.querySelector(".check").addEventListener("click", function () {
   const guess = Number(document.querySelector(".guess").value);
@@ -25,6 +26,11 @@ document.querySelector(".check").addEventListener("click", function () {
     document.querySelector("body").style.backgroundColor = "#60b347";
     document.querySelector(".number").style.width = "30rem";
     document.querySelector(".number").textContent = secretNumber;
+
+    if (score > highscore) {
+      highscore = score;
+      document.querySelector(".highscore").textContent = highscore;
+    }
   } else if (guess > secretNumber) {
     //When guess is too high
 
@@ -60,4 +66,5 @@ document.querySelector(".again").addEventListener("click", function () {
   document.querySelector(".guess").value = "";
   document.querySelector("body").style.backgroundColor = "#222";
   document.querySelector(".number").textContent = "?";
+  document.querySelector(".number").style.width = "15rem";
 });
