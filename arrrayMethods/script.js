0cd2ad9d9f0e6dee211ab8bf8c993625e2d38b41
rebecca <185906896+trustnoreb@users.noreply.github.com>
2024-12-04 21:39:34 +0100

    New Methods Arrays

--- arrrayMethods/script.js
+++ arrrayMethods/script.js
@@ -16,18 +16,32 @@
 // console.log(arr);
 
 //reverse
-let arr = ["a", "b", "c", "d", "e"];
-const arr2 = ["j", "i", "h", "g", "f"];
-console.log(arr2.reverse());
+// let arr = ["a", "b", "c", "d", "e"];
+// const arr2 = ["j", "i", "h", "g", "f"];
+// console.log(arr2.reverse());
 
 //concat
-const letters = arr.concat(arr2);
-console.log(letters);
-console.log("arr", arr);
-console.log("arr2", arr2);
-console.log(...arr, ...arr2);
-console.log("arr", arr);
-console.log("arr2", arr2);
+// const letters = arr.concat(arr2);
+// console.log(letters);
+// console.log("arr", arr);
+// console.log("arr2", arr2);
+// console.log(...arr, ...arr2);
+// console.log("arr", arr);
+// console.log("arr2", arr2);
 
 //join
-console.log(letters.join(" - "));
+// console.log(letters.join(" - "));
+
+//-------------------------------------------------
+
+//The new at method
+const arr = [23, 11, 64];
+console.log(arr[0]);
+console.log(arr.at(0));
+
+//TRADITIONAL METHOD: Getting the  array last element
+console.log(arr[arr.length - 1]);
+console.log(arr.slice(-1));
+
+//NEW METHOD: Getting the  array last element
+console.log(arr.at(-1));
